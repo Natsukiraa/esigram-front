b026540e0f5bdf73d7238a3db6afa124fb1ced19
Akas <78977104+Akasuraax@users.noreply.github.com>
2025-11-02 16:37:13 +0100

    feat: update AuthScreen with new UI elements and welcome messages

--- app/src/main/java/com/example/esigram/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/esigram/ui/screens/HomeScreen.kt
@@ -5,14 +5,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.esigram.ui.components.MainMenuTopBar
 import com.example.esigram.viewModels.AuthViewModel
-import com.example.esigram.viewModels.CompleteProfileViewModel
 import com.example.esigram.viewModels.ConversationViewModel
 
 @Composable
 fun HomeScreen(
     authViewModel: AuthViewModel,
     convViewModel: ConversationViewModel,
-    completeProfileViewModel: CompleteProfileViewModel,
     onSignOut: () -> Unit) {
     Column {
         MainMenuTopBar(viewModel = authViewModel, onSignOut = onSignOut)
