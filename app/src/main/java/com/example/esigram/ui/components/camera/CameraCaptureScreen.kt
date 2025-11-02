8a771510c53de88422e04f16109aa22e2d7089b9
Natsukiraa <gazio.mael@gmail.com>
2025-11-02 18:07:40 +0100

    add CameraCaptureScreen composable for managing camera viewfinder and controls

---
+++ app/src/main/java/com/example/esigram/ui/components/camera/CameraCaptureScreen.kt
@@ -0,0 +1,40 @@
+package com.example.esigram.ui.components.camera
+
+import androidx.camera.compose.CameraXViewfinder
+import androidx.camera.core.SurfaceRequest
+import androidx.camera.video.Recording
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CameraCaptureScreen(
+    surfaceRequest: SurfaceRequest?,
+    isRecording: Boolean,
+    onCapture: () -> Unit,
+    onToggleRecord: () -> Unit,
+    onSelectFromGallery: () -> Unit
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        if (surfaceRequest != null) {
+            CameraXViewfinder(
+                surfaceRequest = surfaceRequest,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+
+        CameraControls(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 32.dp),
+            onCapture = onCapture,
+            toggleRecord = onToggleRecord,
+            activeRecording = isRecording,
+            onSelectFromGallery = onSelectFromGallery
+        )
+    }
+}
