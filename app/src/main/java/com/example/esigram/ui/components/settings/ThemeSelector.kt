dadd2f12759054308ca4655b02fd87469cdfb0ef
Fantom <arthur.morelon@gmail.com>
2025-12-13 13:21:39 +0100

    add LanguageSelector

--- app/src/main/java/com/example/esigram/ui/components/settings/ThemeSelector.kt
+++ app/src/main/java/com/example/esigram/ui/components/settings/ThemeSelector.kt
@@ -1,13 +1,10 @@
-package com.example.esigram.ui.components
+package com.example.esigram.ui.components.settings
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -24,7 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.palette.graphics.Palette
 import com.example.esigram.R
 import com.example.esigram.domains.models.ThemeMode
 
