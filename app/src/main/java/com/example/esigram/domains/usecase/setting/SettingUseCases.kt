668a60cd35af4cad747e5d467302d3caddc3aa9c
Fantom <arthur.morelon@gmail.com>
2025-12-13 13:11:40 +0100

    add locale UseCases

--- app/src/main/java/com/example/esigram/domains/usecase/setting/SettingUseCases.kt
+++ app/src/main/java/com/example/esigram/domains/usecase/setting/SettingUseCases.kt
@@ -2,5 +2,7 @@ package com.example.esigram.domains.usecase.setting
 
 data class SettingUseCases(
     val getThemeMode: GetThemeUseCase,
-    val setThemeMode: SetThemeUseCase
+    val setThemeMode: SetThemeUseCase,
+    val getLocale: GetLocaleUseCase,
+    val setLocale: SetLocaleUseCase
 )
